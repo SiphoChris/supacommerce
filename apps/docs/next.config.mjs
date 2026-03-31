266272e6b10a43bc9b2828bfa8f61ea1463bb526
sinkecpt025 <sinkecpt025@student.wethinkcode.co.za>
2026-03-31 10:52:46 +0200

    update: supacommerce docs

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -1,11 +1,37 @@
+// import { createMDX } from 'fumadocs-mdx/next';
+
+// const withMDX = createMDX();
+
+// /** @type {import('next').NextConfig} */
+// const config = {
+//   serverExternalPackages: ['@takumi-rs/image-response'],
+//   reactStrictMode: true,
+//   async rewrites() {
+//     return [
+//       {
+//         source: '/docs/:path*.mdx',
+//         destination: '/llms.mdx/docs/:path*',
+//       },
+//     ];
+//   },
+// };
+
+// export default withMDX(config);
+
+
+
+import path from 'path';
+import { fileURLToPath } from 'url';
 import { createMDX } from 'fumadocs-mdx/next';
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
   serverExternalPackages: ['@takumi-rs/image-response'],
   reactStrictMode: true,
+  outputFileTracingRoot: path.join(__dirname, '../../'),
   async rewrites() {
     return [
       {
